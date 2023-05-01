279cd5a1cf4f131b7c6bfb10e2436af32871daf5
dormyyy <sania.crafter@gmail.com>
2023-05-01 13:00:24 +0300

    minor_fix

--- handlers/users/subscription.py
+++ handlers/users/subscription.py
@@ -17,6 +17,8 @@ async def bot_subscription(message: types.Message):
             [types.InlineKeyboardButton(text='Увімкнути', callback_data='on')]
         ], resize_keyvoard=True
     )
+    keyboard_on.inline_keyboard.extend([[types.InlineKeyboardButton(text='Не змінювати', callback_data='cancel')]])
+    keyboard_off.inline_keyboard.extend([[types.InlineKeyboardButton(text='Не змінювати', callback_data='cancel')]])
     user = session.query(User).filter_by(telegram_id=message.from_user.id).first()
     if user.subscribed:
         await message.answer("На разі ваші повідомлення увімкнені, якщо ви бажаєте їх вимкнути, натисніть «Вимкнути»",
@@ -24,6 +26,13 @@ async def bot_subscription(message: types.Message):
     else:
         await message.answer("На разі ваші повідомлення вимкнені, якщо ви бажаєте їх увімкнути, натисніть «Увімкнути»",
                              reply_markup=keyboard_on)
+
+
+@dp.callback_query_handler(text='cancel', state=States.subscriptionState)
+async def bot_cancel_bot_subscription(callback_query: types.CallbackQuery):
+    await bot.send_message(callback_query.from_user.id,
+                           "Повідомлення не змінені. Чекайте своїх новин!")
+    await States.startState.set()
 
 
 @dp.callback_query_handler(lambda x: x.data, state=States.subscriptionState)
